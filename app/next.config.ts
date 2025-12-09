dd4f70704c522a85d928b8176972da751c6d954b
eltonanit <eltonanit@gmail.com>
2025-12-09 23:59:39 +0100

    ix: recreate next.co

---
+++ app/next.config.ts
@@ -0,0 +1,20 @@
+﻿import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
